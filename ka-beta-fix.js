2c93b7e1026bef3ed32fed956f2763e3e45727ea
Khada <khadakuraki@gmail.com>
2018-02-13 01:14:51 +1100

    Added updateURL flag and inc ver

--- ka-beta-fix.js
+++ ka-beta-fix.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Kiss Anime Beta Server Fix
 // @namespace    Khada
-// @version      0.2
+// @version      0.3
 // @description  Auto add "&pfail=1" to KA vid urls ending in "&s=beta" or "&s=default".
 // @author       khadakuraki@gmail.com
 // @match        http://*kissanime.ru/*&s=beta
 // @match        http://*kissanime.ru/*&s=default
 // @exclude      http://*kissanime.ru/*&pfail=1
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/JoshuaCarter/tamper-monkey-ka-beta-fix/master/ka-beta-fix.js
 // ==/UserScript==
 
 (function() {
