b83a57d5cc9c925bdbe555d22d8d33a31aeebb02
Joshua Carter <joshua.carter.dev@gmail.com>
2018-02-13 03:39:38 +1100

    added icon

--- ka-beta-fix.js
+++ ka-beta-fix.js
@@ -9,6 +9,7 @@
 // @exclude      http://*kissanime.ru/*&pfail=1
 // @grant        none
 // @run-at       document-start
+// @icon         http://kissanime.ru/Content/images/favicon.ico
 // @homepageURL  https://github.com/JoshuaCarter/tamper-monkey-ka-beta-fix
 // @supportURL   https://github.com/JoshuaCarter/tamper-monkey-ka-beta-fix/issues
 // @downloadURL  https://raw.githubusercontent.com/JoshuaCarter/tamper-monkey-ka-beta-fix/master/ka-beta-fix.js
